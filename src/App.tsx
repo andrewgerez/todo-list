121439644ccd7f7a92685dd2984296d2d1148cc0
Andrew Gerez <andrewgerez18@gmail.com>
2023-09-19 21:03:14 -0300

    feat: global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "./styles/theme/default";
+import { GlobalStyle } from "./styles/global";
+
 export function App() {
   return (
-    <div>start :p</div>
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+    </ThemeProvider>
   )
 }
